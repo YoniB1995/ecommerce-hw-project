82f00dcf3413ea46c5e9e9e5b54394cf91a34346
YoniB1995 <yonatansamfisher@gmail.com>
2021-09-14 20:17:45 +0300

    updated api services routes

--- client/src/service/api.service.js
+++ client/src/service/api.service.js
@@ -1,10 +1,10 @@
 
 export const API = "https://milk-n-honey-store-app.herokuapp.com/products"
 
-export const API_STORE_MEN="https://milk-n-honey-store-app.herokuapp.com/category/men"
+export const API_STORE_MEN="https://milk-n-honey-store-app.herokuapp.com/products/category/men"
 
-export const API_STORE_WOMEN="https://milk-n-honey-store-app.herokuapp.com/category/women"
+export const API_STORE_WOMEN="https://milk-n-honey-store-app.herokuapp.com/products/category/women"
 
-export const API_STORE_ELECTRONICS="https://milk-n-honey-store-app.herokuapp.com/category/electronics"
+export const API_STORE_ELECTRONICS="https://milk-n-honey-store-app.herokuapp.com/products/category/electronics"
 
-export const API_STORE_JEWELRY="https://milk-n-honey-store-app.herokuapp.com/category/jewelry"
+export const API_STORE_JEWELRY="https://milk-n-honey-store-app.herokuapp.com/category/products/jewelry"
