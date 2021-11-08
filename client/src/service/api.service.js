ee1a3107899c5aeaea239e3c0e8f01016279d3ef
YoniB1995 <yonatansamfisher@gmail.com>
2021-11-08 10:22:20 +0200

    fixed api route for backend

--- client/src/service/api.service.js
+++ client/src/service/api.service.js
@@ -1,9 +1,9 @@
 
-export const API = "https://milk-n-honey-store-app.herokuapp.com/products"
-export const API_STORE_MEN="https://milk-n-honey-store-app.herokuapp.com/products/category/men"
-export const API_STORE_WOMEN="https://milk-n-honey-store-app.herokuapp.com/products/category/women"
-export const API_STORE_ELECTRONICS="https://milk-n-honey-store-app.herokuapp.com/products/category/electronics"
-export const API_STORE_JEWELRY="https://milk-n-honey-store-app.herokuapp.com/products/category/jewelry"
+export const API = "https://ecommerce-hw-project.herokuapp.com/products"
+export const API_STORE_MEN="https://ecommerce-hw-project.herokuapp.com/products/category/men"
+export const API_STORE_WOMEN="https://ecommerce-hw-project.herokuapp.com/products/category/women"
+export const API_STORE_ELECTRONICS="https://ecommerce-hw-project.herokuapp.com/products/category/electronics"
+export const API_STORE_JEWELRY="https://ecommerce-hw-project.herokuapp.com/products/category/jewelry"
 
 
 export const API_LOCAL = "http://localhost:5000/products"
